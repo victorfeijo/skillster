269a8b6b5aa9191a92a76d921874a3c16a5f679b
Victor Feijo <victor.feijoa@gmail.com>
2018-05-14 21:30:06 -0300

    Change tasks data by index

--- src/containers/NewTrainContainer.tsx
+++ src/containers/NewTrainContainer.tsx
@@ -1,6 +1,7 @@
-import * as React from "react";
-import { Platform, StyleSheet, View } from "react-native";
-import { Icon, Button, Content, Card, CardItem, Body, Text, Form, Item, Label, Input } from "native-base";
+import * as React from 'react';
+import { remove } from 'ramda';
+import { Platform, StyleSheet, View } from 'react-native';
+import { Icon, Button, Content, Card, CardItem, Body, Text, Form, Item, Label, Input } from 'native-base';
 
 import { Provider, Dispatch, connect } from 'react-redux';
 import foo from '../reducers';
@@ -33,9 +34,6 @@ class NewTrainContainer extends React.Component<{}, {}> {
     tasks: [{
       name: '',
       tries: '',
-    }, {
-      name: '',
-      tries: '',
     }],
   };
 
@@ -43,16 +41,35 @@ class NewTrainContainer extends React.Component<{}, {}> {
     this.setState({ name: value });
   }
 
-  renderTry = () => (
-    <Card>
-      <Button style={styles.closeBtn} transparent={true} danger={true}>
+  removeIdxTask = (idx: number) => (
+    () => {
+      const { tasks } = this.state;
+
+      if (tasks.length > 1) {
+        this.setState({ tasks: remove(idx, 1, this.state.tasks) });
+      }
+    }
+  )
+
+  onChangeIdxTaskName = (idx: number) => (
+    (text: string) => {
+      const { tasks } = this.state;
+      tasks[idx].name = text;
+
+      this.setState({ tasks });
+    }
+  )
+
+  renderTaskForm = (task: object, idx: number) => (
+    <Card key={idx}>
+      <Button style={styles.closeBtn} transparent={true} danger={true} onPress={this.removeIdxTask(idx)}>
         <Icon type="FontAwesome" name="close" />
       </Button>
 
       <CardItem>
         <Item floatingLabel={true}>
           <Label>Task name</Label>
-          <Input />
+          <Input onChangeText={this.onChangeIdxTaskName(idx)} value={this.state.tasks[idx].name} />
         </Item>
       </CardItem>
 
@@ -65,6 +82,10 @@ class NewTrainContainer extends React.Component<{}, {}> {
     </Card>
   )
 
+  addNewTask = () => {
+    this.setState({ tasks: [...this.state.tasks, { name: '', tries: ''}] });
+  }
+
   render() {
     return (
       <Content>
@@ -74,8 +95,11 @@ class NewTrainContainer extends React.Component<{}, {}> {
             <Input value={this.state.name} onChangeText={this.onChangeName} />
           </Item>
 
-          { this.state.tasks.map((v) => this.renderTry()) }
+          {this.state.tasks.map((task, idx) => this.renderTaskForm(task, idx))}
 
+          <Button bordered={true} success={true} onPress={this.addNewTask}>
+            <Text>Add Task</Text>
+          </Button>
         </Form>
       </Content>
     );
